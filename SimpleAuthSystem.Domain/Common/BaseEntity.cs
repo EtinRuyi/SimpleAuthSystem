fb3635b6fbcab2bd587015843cfd9d7fadd274ce
EtinRuyi <omoruyi.idowu@yahoo.com>
2025-05-12 01:09:20 +0100

    DomainSetup/fix

---
+++ SimpleAuthSystem.Domain/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleAuthSystem.Domain.Common
+{
+    public class BaseEntity
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? LastLogin { get; set; } = DateTime.UtcNow;
+        public bool? IsDeleted { get; set; } = false;
+    }
+}
---
+++ SimpleAuthSystem.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using SimpleAuthSystem.Domain.Entities;
+
+namespace SimpleAuthSystem.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<AppUser> GetByUsernameAsync(string username);
+        Task<AppUser> GetByEmailAsync(string email);
+        Task<AppUser> GetByIdAsync(string id);
+        Task AddAsync(AppUser user);
+    }
+}
